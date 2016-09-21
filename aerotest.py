85c40362e611ccf26997090697963ab520c05dc7
Shen Ge <shenge86@gmail.com>
2016-09-20 19:03:43 -0500

    Testing file
    ### Tests aerospace library ###
    This file should be outside of the aerolibb library to test the library

---
+++ aerotest.py
@@ -0,0 +1,45 @@
+### Tests aerospace library ###
+import sys
+# This file should be outside of the aerolibb library
+# Change path depending on your own system
+sys.path.insert(0, r"C:\Users\sheng\Dropbox\SG\Python\LIBRARIES\aerolibb") #the directory that contains my_pkg
+import numpy as np
+# from aerolibb import propulsion
+# from aerolibb import framechange
+from aerolibb import spacecraft
+# propulsion as aero
+
+deltav = 3000
+isp = 350
+mi = 50000
+# (mf, mp) = propulsion.rocketeqn(deltav, isp, mi)
+
+# print(mf)
+# print(mp)
+
+
+# rp = np.array([[1], [1], [1]])
+# rotMatrix = framechange.euler(np.pi/4, 0, 0)
+# print(rotMatrix)
+# print(rp)
+# rs = np.dot(rotMatrix, rp)
+# r2 = np.matrix([[1, 1, 1], [1, 2, 1]])
+#rs = np.dot(r2,rp)
+
+# rs = np.dot(rotMatrix, rp)
+# print(rs)
+
+# Test #
+sc = spacecraft.spacecraft('lunarcraft',1000,1000,1000)
+sc.printDesc()
+rp = sc.definePosition(1,1,1)
+print("INITIAL CONFIG:")
+sc.printDesc()
+print(rp)
+
+print("ROTATE TO NEW FRAME: ")
+rp = sc.rotateAttitude(0,0,np.pi/4)
+
+print("FINAL CONFIG:")
+print("Positions are now defined relative to new frame.")
+sc.printDesc()
