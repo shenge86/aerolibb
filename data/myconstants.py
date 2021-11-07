3d66d5f666bc31a778511cd51a53a9ae21c8e0da
shenge86 <shenge86@gmail.com>
2021-11-07 17:02:20 -0600

    Add moon constant

--- data/myconstants.py
+++ data/myconstants.py
@@ -11,5 +11,8 @@ g = 9.8 # m/s2 (gravity on surface)
 iE = 28.5  # degrees (inclination of Earth)
 AU = 1.49598 * 10 ** 8 # km (avg distance from Earth to sun)
 
+# Moon parameters
+gmoon = g/6
+
 # other
 solarconstant = 1371 # W/m2 (solar constant)
